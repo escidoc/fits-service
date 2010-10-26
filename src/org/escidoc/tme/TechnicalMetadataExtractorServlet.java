74003e7e3d8541dd8a8ff925cca686c6b7d500a3
fschwic <frank.schwichtenberg@fiz-karlsruhe.de>
2010-10-26 07:47:03 +0000

    organized imports

--- src/org/escidoc/tme/TechnicalMetadataExtractorServlet.java
+++ src/org/escidoc/tme/TechnicalMetadataExtractorServlet.java
@@ -1,17 +1,7 @@
 package org.escidoc.tme;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.Charset;
-import java.util.zip.Adler32;
-import java.util.zip.Checksum;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
