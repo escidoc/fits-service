7e91c000dc26bcb8d4212688932442458f735eea
fschwic <frank.schwichtenberg@fiz-karlsruhe.de>
2010-11-04 13:59:24 +0000

    revised error responses

--- src/org/escidoc/tme/TechnicalMetadataExtractorServlet.java
+++ src/org/escidoc/tme/TechnicalMetadataExtractorServlet.java
@@ -13,44 +13,51 @@ import org.escidoc.tme.fits.FitsMetadataExtractor;
 
 public class TechnicalMetadataExtractorServlet extends HttpServlet {
 
-    private static final long serialVersionUID = -2124267350610278867L;
+	private static final long serialVersionUID = -2124267350610278867L;
 
-    /**
-     * Implements the HTTP get-request
-     * 
-     * @param req
-     *            HttpServletRequest
-     * @param resp
-     *            HttpServletResponse
-     */
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-        throws ServletException, IOException {
+	/**
+	 * Implements the HTTP get-request
+	 * 
+	 * @param req
+	 *            HttpServletRequest
+	 * @param resp
+	 *            HttpServletResponse
+	 */
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
 
-        String path = req.getParameter("path");
-        if (path == null || path.trim().length() == 0) {
-            String msg =
-                "Parameter 'path' is required. Try .../examine?path=[local-path|remote-path]";
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, msg);
-        }
-        else {
+		String path = req.getParameter("path");
+		if (path == null || path.trim().length() == 0) {
+			String msg = "Parameter 'path' is required. Try .../examine?path=[local-path|remote-path]";
+			resp.sendError(HttpServletResponse.SC_BAD_REQUEST, msg);
+		} else {
 
-            String xml = null;
-            resp.setContentType("text/xml");
+			String xml = null;
+			resp.setContentType("text/xml");
 
-            try {
-                // TODO factory?
-                MetadataExtractor me = new FitsMetadataExtractor();
-                xml = me.extractMetadata(path);
-            }
-            catch (MetadataExtractorException e) {
-                throw new ServletException(e);
-            }
+			try {
+				// TODO factory?
+				MetadataExtractor me = new FitsMetadataExtractor();
+				xml = me.extractMetadata(path);
+			} catch (MetadataExtractorException e) {
+				String msg = "Error extracting metadata: "
+						+ e.getLocalizedMessage();
+				resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+						msg);
+				// throw new ServletException(e);
+			} catch (IOException e) {
+				String msg = "Input/Output Error extracting metadata: "
+						+ e.getLocalizedMessage();
+				resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+						msg);
+				// throw new ServletException(e);
+			}
 
-            if (xml != null) {
-                PrintWriter w = resp.getWriter();
-                w.println(xml);
-            }
-        }
-    }
+			if (xml != null) {
+				PrintWriter w = resp.getWriter();
+				w.println(xml);
+			}
+		}
+	}
 
 }
