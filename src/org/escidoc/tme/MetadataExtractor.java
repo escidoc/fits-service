2fd28075d516fc389415c1d76af3d880b02b9d43
fschwic <frank.schwichtenberg@fiz-karlsruhe.de>
2010-11-04 13:58:18 +0000

    documented NullPointerException

--- src/org/escidoc/tme/MetadataExtractor.java
+++ src/org/escidoc/tme/MetadataExtractor.java
@@ -6,20 +6,22 @@ import org.escidoc.tme.exceptions.MetadataExtractorException;
 
 public interface MetadataExtractor {
 
-    /**
-     * Extracts technical metadata from file specified by locator. Which kinds
-     * of locator are accepted depends on the concrete MetadataExtractor.
-     * 
-     * @param locator
-     *            The location of the file to extract technical metadata from.
-     * @return A string containing an XML document. TODO better return XML
-     *         document because of charset?
-     * @throws MetadataExtractorException
-     *             If an error occurs extracting metadata.
-     * @throws IOException
-     *             If the file can not be read.
-     */
-    public String extractMetadata(String locator) throws MetadataExtractorException,
-        IOException;
+	/**
+	 * Extracts technical metadata from file specified by locator. Which kinds
+	 * of locator are accepted depends on the concrete MetadataExtractor.
+	 * 
+	 * @param locator
+	 *            The location of the file to extract technical metadata from.
+	 * @return A string containing an XML document. TODO better return XML
+	 *         document because of charset?
+	 * @throws MetadataExtractorException
+	 *             If an error occurs extracting metadata.
+	 * @throws IOException
+	 *             If the file can not be read.
+	 * @throws NullPointerException
+	 *             If locator is null.
+	 */
+	public String extractMetadata(String locator)
+			throws MetadataExtractorException, IOException;
 
 }
